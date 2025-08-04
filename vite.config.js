b40979a8f0c33ca5245bc4161974abaf05de26eb
NS <2300030644@kluniversity.in>
2025-08-04 11:16:25 +0530

    changed scripts in package.json

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'jenkinserp',// deployment folder name but not git repo name
+  base:'/jenkinserp/',// deployment folder name but not git repo name
 })
